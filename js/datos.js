ac1906b73975fabede5cd4eab6cbb97be983b103
Santiago Llovera <105535098+sllovera91@users.noreply.github.com>
2022-05-18 19:05:32 -0300

    Update datos.js

--- js/datos.js
+++ js/datos.js
@@ -2,7 +2,7 @@ const app = new Vue({
   el: '#app',
   data: {
         tratamientos: [
-        {imagen: "https://raw.githubusercontent.com/sllovera91/estetika/main/img/adip.jpgg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
+        {imagen: "https://raw.githubusercontent.com/sllovera91/estetika/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
         {imagen: "https://raw.githubusercontent.com/sllovera91/estetika/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
         {imagen: "https://raw.githubusercontent.com/sllovera91/estetika/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
         {imagen: "https://raw.githubusercontent.com/sllovera91/estetika/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."}, 
