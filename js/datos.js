d5f497a744cd3bc906d14fafd785928728e2018b
Santiago Llovera <105535098+sllovera91@users.noreply.github.com>
2022-05-18 18:35:37 -0300

    Update datos.js

--- js/datos.js
+++ js/datos.js
@@ -2,12 +2,12 @@ const app = new Vue({
   el: '#app',
   data: {
         tratamientos: [
-        {imagen: "https://github.com/sllovera91/estetika/blob/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
-        {imagen: "https://github.com/sllovera91/estetika/blob/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
-        {imagen: "https://github.com/sllovera91/estetika/blob/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
-        {imagen: "https://github.com/sllovera91/estetika/blob/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."}, 
-        {imagen: "https://github.com/sllovera91/estetika/blob/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
-        {imagen: "https://github.com/sllovera91/estetika/blob/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."}, 
+        {imagen: "https://raw.githubusercontent.com/sllovera91/estetika/main/img/adip.jpgg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
+        {imagen: "https://raw.githubusercontent.com/sllovera91/estetika/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
+        {imagen: "https://raw.githubusercontent.com/sllovera91/estetika/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
+        {imagen: "https://raw.githubusercontent.com/sllovera91/estetika/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."}, 
+        {imagen: "https://raw.githubusercontent.com/sllovera91/estetika/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."},
+        {imagen: "https://raw.githubusercontent.com/sllovera91/estetika/main/img/adip.jpg", nombre: 'Tratamiento', descripcion:"Aqui va un descripcion acerca del tratamiento ofrecido..."}, 
       ]
      }
   });
